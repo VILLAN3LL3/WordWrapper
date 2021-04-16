312d46e43abd62e31c606bdeeff369574027772b
Mira Manger <mira.manger@haufe-lexware.com>
2021-04-16 15:40:45 +0200

    use environment.newline in test

--- WordWrapper.Tests/InteractorTests.cs
+++ WordWrapper.Tests/InteractorTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace WordWrapper.Tests
@@ -12,48 +13,79 @@ namespace WordWrapper.Tests
         public void WrapWords_StateUnderTest_ExpectedBehavior()
         {
             Interactor interactor = CreateInteractor();
-            const string text = "Curabitur, scelerisque magnis porttitor condimentum vestibulum sapien scelerisque malesuada natoque ultrices per, ante\r\n" +
-                "interdum conubia phasellus eget. Eleifend dignissim.\r\n" +
-                "\r\n" +
-                "Dignissim facilisis quam taciti fusce metus amet litora quisque turpis blandit sed. Suspendisse natoque proin nunc primis fusce.\r\n" +
-                "\r\n" +
-                "Molestie. Duis pede senectus nunc integer sed Porttitor lectus dapibus vivamus id dolor\r\n" +
-                "sollicitudin nascetur, facilisi gravida non habitant tristique velit.\r\n" +
-                "\r\n" +
-                "Magnis. Suspendisse, rutrum sit Justo nisl nisi malesuada aenean aptent sed primis. Tempor fames erat tortor.\r\n" +
-                "\r\n" +
-                "Lobortis potenti curabitur, tellus habitant, hymenaeos rutrum erat nunc sodales natoque conubia\r\n" +
-                "hendrerit quam aptent mollis sit aenean enim vivamus felis, feugiat massa duis ullamcorper bibendum.\r\n" +
-                "\r\n" +
+            string text = "Curabitur, scelerisque magnis porttitor condimentum vestibulum sapien scelerisque malesuada natoque ultrices per, ante" +
+                Environment.NewLine +
+                "interdum conubia phasellus eget. Eleifend dignissim." +
+                Environment.NewLine +
+                Environment.NewLine +
+                "Dignissim facilisis quam taciti fusce metus amet litora quisque turpis blandit sed. Suspendisse natoque proin nunc primis fusce." +
+                Environment.NewLine +
+                Environment.NewLine +
+                "Molestie. Duis pede senectus nunc integer sed Porttitor lectus dapibus vivamus id dolor" +
+                Environment.NewLine +
+                "sollicitudin nascetur, facilisi gravida non habitant tristique velit." +
+                Environment.NewLine +
+                Environment.NewLine +
+                "Magnis. Suspendisse, rutrum sit Justo nisl nisi malesuada aenean aptent sed primis. Tempor fames erat tortor." +
+                Environment.NewLine +
+                Environment.NewLine +
+                "Lobortis potenti curabitur, tellus habitant, hymenaeos rutrum erat nunc sodales natoque conubia" +
+                Environment.NewLine +
+                "hendrerit quam aptent mollis sit aenean enim vivamus felis, feugiat massa duis ullamcorper bibendum." +
+                Environment.NewLine +
+                Environment.NewLine +
                 "Lobortispotenticurabiturtellushabitanthymenaeosrutrumerat";
-            const string expectedText = "Curabitur, scelerisque magnis porttitor\r\n" +
-                "condimentum vestibulum sapien\r\n" +
-                "scelerisque malesuada natoque ultrices\r\n" +
-                "per, ante interdum conubia phasellus\r\n" +
-                "eget. Eleifend dignissim.\r\n" +
-                "\r\n" +
-                "Dignissim facilisis quam taciti fusce\r\n" +
-                "metus amet litora quisque turpis blandit\r\n" +
-                "sed. Suspendisse natoque proin nunc\r\n" +
-                "primis fusce.\r\n" +
-                "\r\n" +
-                "Molestie. Duis pede senectus nunc\r\n" +
-                "integer sed Porttitor lectus dapibus\r\n" +
-                "vivamus id dolor sollicitudin nascetur,\r\n" +
-                "facilisi gravida non habitant tristique\r\n" +
-                "velit.\r\n" +
-                "\r\n" +
-                "Magnis. Suspendisse, rutrum sit Justo\r\n" +
-                "nisl nisi malesuada aenean aptent sed\r\n" +
-                "primis. Tempor fames erat tortor.\r\n" +
-                "\r\n" +
-                "Lobortis potenti curabitur, tellus\r\n" +
-                "habitant, hymenaeos rutrum erat nunc\r\n" +
-                "sodales natoque conubia hendrerit quam\r\n" +
-                "aptent mollis sit aenean enim vivamus\r\n" +
-                "felis, feugiat massa duis ullamcorper\r\n" +
-                "bibendum.\r\n" +
-                "\r\n" +
+            string expectedText = "Curabitur, scelerisque magnis porttitor" +
+                Environment.NewLine +
+                "condimentum vestibulum sapien" +
+                Environment.NewLine +
+                "scelerisque malesuada natoque ultrices" +
+                Environment.NewLine +
+                "per, ante interdum conubia phasellus" +
+                Environment.NewLine +
+                "eget. Eleifend dignissim." +
+                Environment.NewLine +
+                Environment.NewLine +
+                "Dignissim facilisis quam taciti fusce" +
+                Environment.NewLine +
+                "metus amet litora quisque turpis blandit" +
+                Environment.NewLine +
+                "sed. Suspendisse natoque proin nunc" +
+                Environment.NewLine +
+                "primis fusce." +
+                Environment.NewLine +
+                Environment.NewLine +
+                "Molestie. Duis pede senectus nunc" +
+                Environment.NewLine +
+                "integer sed Porttitor lectus dapibus" +
+                Environment.NewLine +
+                "vivamus id dolor sollicitudin nascetur," +
+                Environment.NewLine +
+                "facilisi gravida non habitant tristique" +
+                Environment.NewLine +
+                "velit." +
+                Environment.NewLine +
+                Environment.NewLine +
+                "Magnis. Suspendisse, rutrum sit Justo" +
+                Environment.NewLine +
+                "nisl nisi malesuada aenean aptent sed" +
+                Environment.NewLine +
+                "primis. Tempor fames erat tortor." +
+                Environment.NewLine +
+                Environment.NewLine +
+                "Lobortis potenti curabitur, tellus" +
+                Environment.NewLine +
+                "habitant, hymenaeos rutrum erat nunc" +
+                Environment.NewLine +
+                "sodales natoque conubia hendrerit quam" +
+                Environment.NewLine +
+                "aptent mollis sit aenean enim vivamus" +
+                Environment.NewLine +
+                "felis, feugiat massa duis ullamcorper" +
+                Environment.NewLine +
+                "bibendum." +
+                Environment.NewLine +
+                Environment.NewLine +
                 "Lobortispotenticurabiturtellushabitanthymenaeosrutrumerat";
             const int width = 40;
 
