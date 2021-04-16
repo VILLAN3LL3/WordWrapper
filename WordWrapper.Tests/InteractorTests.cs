46dde8f754914df52d2099b97b5740599d2a56cf
Mira Manger <mira.manger@haufe-lexware.com>
2021-04-16 13:56:01 +0200

    added integration test

---
+++ WordWrapper.Tests/InteractorTests.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace WordWrapper.Tests
+{
+    [TestFixture]
+    public class InteractorTests
+    {
+
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        private Interactor CreateInteractor()
+        {
+            return new Interactor();
+        }
+
+        [Test]
+        public void WrapWords_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var interactor = this.CreateInteractor();
+            string text = File.ReadAllText("text.txt");
+            string expectedText = File.ReadAllText("expected_result.txt");
+            int width = 40;
+
+            // Act
+            var result = interactor.WrapWords(
+                text,
+                width);
+
+            // Assert
+            result.Should().Be(expectedText);
+        }
+    }
+}
---
+++ WordWrapper/Interactor.cs
@@ -0,0 +1,10 @@
+﻿namespace WordWrapper
+{
+    public class Interactor
+    {
+        public string WrapWords(string text, int width)
+        {
+            return text;
+        }
+    }
+}
